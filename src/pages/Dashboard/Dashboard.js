01dc9f18611699ff5a5ec1d426208cc9d23695b9
rafaathzanar <zanarmhmra.21@uom.lk>
2024-02-07 07:44:44 +0530

    Starting Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,31 +2,7 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 
 function Dashboard() {
-  console.log("Rendering DAshboardssssss");
-  return (
-    <Grid container spacing={2} item xs={12}>
-      This is Dac paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis
-      is Dac paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is
-      Dac paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadgThis is Dac
-      paasdfsdfsdfsdfsdfsdfsdfsdfasdfasdfsdfsdfdsfdsfsdfge.gsadg
-    </Grid>
-  );
+  return <Grid container spacing={2} item xs={12}></Grid>;
 }
 
 export default Dashboard;
