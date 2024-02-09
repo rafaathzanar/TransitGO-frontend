8d05137d24ff33e4e1ae29417019681d81e1ee65
ZulfaTh <zulfamunaf@gmailcom>
2024-02-09 22:15:02 +0530

    modified to responsive

--- src/pages/LostFound/LostFound.js
+++ src/pages/LostFound/LostFound.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import CardBox from "../../components/CardBox";
 import Typography from "@mui/material/Typography";
@@ -5,53 +6,26 @@ import img1 from "../../images/LostAnno.png";
 import img2 from "../../images/FoundAnno.png";
 import img3 from "../../images/Report.png";
 import { useNavigate } from "react-router";
+import { Grid } from "@mui/material";
 
 export default function LostFound() {
   const navigate = useNavigate();
+
   return (
     <div>
-      <Typography
-        variant="h2"
-        align="center"
-        marginTop="50px"
-        sx={{
-          fontSize: 32,
-        }}
-      >
+      <Typography variant="h2" align="center" marginTop="50px">
         Get Your Things Back with
       </Typography>
 
-      <Typography
-        variant="h2"
-        align="center"
-        sx={{
-          fontSize: 32,
-          fontWeight: "bold",
-          fontFamily: "OpenSans",
-        }}
-      >
+      <Typography variant="h2" align="center" fontWeight="bold" fontFamily="OpenSans">
         Lost&Found
       </Typography>
 
-      <div style={{ display: "flex", alignItems: "center", marginTop: "75px" }}>
-        <CardBox
-          icon={img1}
-          typography={"Lost Announcement"}
-          onClick={() => navigate("lostitem")}
-        />
-
-        <CardBox
-          icon={img2}
-          typography={"Found Announcement"}
-          onClick={() => navigate("founditem")}
-        />
-
-        <CardBox
-          icon={img3}
-          typography={"Report Lost/Found"}
-          onClick={() => navigate("lostfoundreport")}
-        />
-      </div>
+      <Grid container spacing={3} justifyContent="center" alignItems="center">
+        <CardBox icon={img1} typography={"Lost Announcement"} onClick={() => navigate("lostitem")} />
+        <CardBox icon={img2} typography={"Found Announcement"} onClick={() => navigate("founditem")} />
+        <CardBox icon={img3} typography={"Report Lost/Found"} onClick={() => navigate("lostfoundreport")} />
+      </Grid>
     </div>
   );
 }
