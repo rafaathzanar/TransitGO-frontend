7287e75b4b9fa1d7c28363528e7eb64c2b253ec0
ZulfaTh <zulfamunaf@gmailcom>
2024-02-09 22:11:44 +0530

    modified to responsive

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./CardBox.css";
+import { Button, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 
 function CardBox({ icon, typography, onClick }) {
   return (
-    <button className="card-box" onClick={onClick}>
-      <div className="card-box-content">
-        <img src={icon} alt={icon} className="card-box-icon" />
-        <span className="card-box-typography">{typography}</span>
-      </div>
-    </button>
+    <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+      <Button className="card-box" onClick={onClick}>
+        <div className="card-box-content">
+          <img src={icon} alt={icon} className="card-box-icon" />
+          <Typography className="card-box-typography">{typography}</Typography>
+        </div>
+      </Button>
+    </Grid>
   );
 }
 
 export default CardBox;
+
