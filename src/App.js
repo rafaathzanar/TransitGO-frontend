29643737b7afd028c0f3ec91bfe8c2a6a671728b
rafaathzanar <zanarmhmra.21@uom.lk>
2024-02-12 15:49:00 +0530

    Appbar Nav Set

--- src/App.js
+++ src/App.js
@@ -2,42 +2,24 @@
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { dashboardTheme } from "./dashboardTheme";
-import HomePage from "./pages/HomePage/HomePage";
-import RouteEditPage from "./pages/RouteEditPage/RouteEditPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminRoutes from "./AdminRoutes";
-import BusSchedule from "./pages/BusSchedule/BusSchedule";
-import ScheduleCard from "./components/ScheduleCard/ScheduleCard";
-import AnnouncementandDelayReportsConductor from "./pages/AnnouncementandDelayReportsConductor/AnnouncementandDelayReportsConductor";
-import AnnouncementandDelayReportsUser from "./pages/AnnouncementandDelayReportsUser/AnnouncementandDelayReportsUser";
-import LostFound from "./pages/LostFound/LostFound";
-import LostItem from "./pages/LostFound/LostItem";
-import FoundItemPage from "./pages/LostFound/FoundItem";
-import FoundForm from "./pages/LostFound/FoundForm";
-import LostForm from "./pages/LostFound/LostForm";
+import PageRoutes from "./PageRoutes";
+import LoginGeneralUser from "./pages/LoginGeneralUser/LoginGeneralUser";
+import SignUp from "./pages/SignUp/SignUp";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import LoginBusEmployee from "./pages/LoginBusEmployee/LoginBusEmployee";
 function App() {
   return (
     <ThemeProvider theme={dashboardTheme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
           <Route path="/admin/*" element={<AdminRoutes />} />
-          <Route path="busschedule" element={<BusSchedule />} />
-          <Route path="/routeeditpage" element={<RouteEditPage />} />
-          <Route path="announcementanddelay">
-            <Route index element={<AnnouncementandDelayReportsConductor />} />
-            <Route
-              path="report"
-              element={<AnnouncementandDelayReportsUser />}
-            />
-          </Route>
-          <Route path="lostandfound">
-            <Route index element={<LostFound />} />
-            <Route path="lostitem" element={<LostItem />} />
-            <Route path="founditem" element={<FoundItemPage />} />
-            <Route path="lostfoundreport" element={<FoundForm />} />
-            <Route path="lostfoundreport2" element={<LostForm />} />
-          </Route>
+          <Route path="/*" element={<PageRoutes />} />
+          <Route path="/signin" element={<LoginGeneralUser />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/signin-employee" element={<LoginBusEmployee />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
