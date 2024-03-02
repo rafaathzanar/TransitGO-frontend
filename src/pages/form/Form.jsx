21fc8d2346066cb5f7cd58b9378c9d37b4720352
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-03-02 13:44:07 +0530

    "Fixes"

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Form.css"
-import {Grid} from "@mui/material";
+import "./Form.css";
+import { Grid } from "@mui/material";
 import H1 from "../../components/heading/heading";
 import M2 from "../../components/assets/p2.png";
 import TextField from "../../components/textfield/Textfield";
@@ -8,17 +8,40 @@ import LoginButton from "../../components/LoginButton/LoginButton";
 
 const Form = () => {
   return (
-    <Grid container >
-      <Grid item className="img1" xs={12} md={6} style={{backgroundColor:" #f1f3f6", paddingTop:100,paddingLeft:50}}>
-        <img  src={M2}/>
+    <Grid container>
+      <Grid
+        item
+        className="img1"
+        xs={12}
+        md={6}
+        style={{
+          backgroundColor: " #f1f3f6",
+          paddingTop: 100,
+          paddingLeft: 50,
+        }}
+      >
+        <img src={M2} />
       </Grid>
       <Grid item xs={12} md={6} mt={10}>
-          <h1 style={{color:"#132968", fontWeight:700, fontSize: 37.813, marginBottom: 30, marginLeft: 150}}>Where is it going?</h1>
-          <div className="textfield"><TextField className="fields"/></div>
-          <div className="confirm"><LoginButton buttonTitle={"Confirm Booking"}/></div>
+        <h1
+          style={{
+            color: "#132968",
+            fontWeight: 700,
+            fontSize: 37.813,
+            marginBottom: 30,
+            marginLeft: 150,
+          }}
+        >
+          Where is it going?
+        </h1>
+        <div className="textfield">
+          <TextField className="fields" />
+        </div>
+        <div className="confirm">
+          <LoginButton buttonTitle={"Confirm Booking"} />
+        </div>
       </Grid>
     </Grid>
-    
   );
 };
 
