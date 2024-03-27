2047a7da3fa062908b51ce646a31771216124d9c
Devindi Peiris <peirismdp.21@uom.lk>
2024-03-27 14:19:45 +0530

    package update

--- src/components/select/selectone.jsx
+++ src/components/select/selectone.jsx
@@ -6,39 +6,49 @@ import Select from '@mui/material/Select';
 
 
 export default function SelectVariants() {
-  const [station, setStation] = React.useState('');
+  // const [station, setStation] = React.useState('');
 
-  const handleChange = (event) => {
-    setStation(event.target.value);
+  // const handleChange = (event) => {
+  //   setStation(event.target.value);
+  // };
+
+  const [fromStation,setFromStation]=React.useState('');
+  const [toStation,setToStation]=React.useState('');
+
+  const handleFromChange = (event)=>{
+    setFromStation(event.target.value);
   };
-
+  
+  const handleToChange = (event)=>{
+    setToStation(event.target.value);
+  };
   return (
     <div>
       <FormControl className='first' sx={{ minWidth: 250, mt:3 }}>
         <InputLabel id="demo-simple-select-standard-label">From</InputLabel>
         <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={station}
-          onChange={handleChange}
-          label="Station"
+          className='from'
+          labelId="from-station-label"
+          id="from-station"
+          value={fromStation}
+          onChange={handleFromChange}
+          label="From Station"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={1}>Colombo</MenuItem>
-          <MenuItem value={2}>Galle</MenuItem>
-          <MenuItem value={3}>Kandy</MenuItem>
+      
         </Select>
       </FormControl>
       <FormControl className='second' sx={{ minWidth: 250, ml:1, mt:3}}>
         <InputLabel id="demo-simple-select-standard-label">To</InputLabel>
         <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={station}
-          onChange={handleChange}
-          label="Station"
+          className='to'
+          labelId="to-station-label"
+          id="to-station"
+          value={toStation}
+          onChange={handleToChange}
+          label="To Station"
         >
           <MenuItem value="">
             <em>None</em>
