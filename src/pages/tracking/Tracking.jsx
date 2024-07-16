12aa081f70a3988a11e15e5b85bd28478c02f714
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-07-16 11:07:23 +0530

    Tracking

--- src/pages/tracking/Tracking.jsx
+++ src/pages/tracking/Tracking.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Heading from "../../components/heading/heading";
 import "./tracking.css";
-import { Stack, TextField, Button } from "@mui/material";
-import map from "../../components/assets/Basemap image.png";
-import Tline from "../../components/Destination/Destination";
+import { TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
 import axios from "axios";
 import trackImage from "../../images/track.png";
 
@@ -12,14 +10,13 @@ function Tracking() {
   const [busID, setBusID] = useState(null);
   const [lastLeftStop, setLastLeftStop] = useState(null);
   const [nextLocation, setNextLocation] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
     if (busID) {
       const fetchTrackingData = async () => {
         try {
-          const response = await axios.get(
-            `http://localhost:8080/bus/${busID}`
-          );
+          const response = await axios.get(`http://localhost:8080/bus/${busID}`);
           setLastLeftStop(response.data.lastLeftStop);
           setNextLocation(response.data.nextLocation);
         } catch (error) {
@@ -33,13 +30,16 @@ function Tracking() {
 
   const handleSearch = async () => {
     try {
-      const packageResponse = await axios.get(
-        `http://localhost:8080/package/${packageID}`
-      );
+      const packageResponse = await axios.get(`http://localhost:8080/package/${packageID}`);
       setBusID(packageResponse.data.busID);
     } catch (error) {
       console.error("Error fetching package data:", error.message);
+      setOpenDialog(true); 
     }
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
   };
 
   return (
@@ -69,13 +69,23 @@ function Tracking() {
         </div>
         <div className="track">
           <div className="location-info">
-            <div className="last-left-stop">
-              Last Left Location : {lastLeftStop}
-            </div>
+            <div className="last-left-stop">Last Left Location : {lastLeftStop}</div>
             <div className="next-location">Next Location : {nextLocation}</div>
           </div>
         </div>
       </div>
+
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>Invalid Package ID</DialogTitle>
+        <DialogContent>
+          The package ID you entered is invalid. Please check and try again.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
